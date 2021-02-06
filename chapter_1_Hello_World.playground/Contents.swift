b7810b7976f5c0783dc0c271c17bd4530a6a742b
Carmen Keating <60234149+ckeating6586@users.noreply.github.com>
2021-02-05 18:28:18 -0800

    Hello World - First Project

---
+++ chapter_1_Hello_World.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Cocoa
+
+var str = "Hello, playground"
+var name = "Carmen"
+var language = "Swift"
+
+var message1 = " Welcome to the wonderful world of"
+var message2 = "\(name), Welcome to the wonderful world of \(language) !"
+
+print(message2)
+print(name, message1, language, "!")
